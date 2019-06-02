2498bc3da66ebde43680d8e974e3ecf3fabf8a96
minhphan2603 <48484833+minhphan2603@users.noreply.github.com>
2019-06-02 23:48:22 +0700

    Updates

---
+++ precache-manifest.20f72fe142b99462764e98e6c35925ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a86fca6924768881ae33901d5c2e116",
+    "url": "/SmartBrain/index.html"
+  },
+  {
+    "revision": "df5a29d344ac7b0db1be",
+    "url": "/SmartBrain/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "d45ac947b4a6e03ff06a",
+    "url": "/SmartBrain/static/css/main.0561267a.chunk.css"
+  },
+  {
+    "revision": "df5a29d344ac7b0db1be",
+    "url": "/SmartBrain/static/js/2.bb448cf8.chunk.js"
+  },
+  {
+    "revision": "d45ac947b4a6e03ff06a",
+    "url": "/SmartBrain/static/js/main.d40afefd.chunk.js"
+  },
+  {
+    "revision": "3355cc07d0f8d622c8fb",
+    "url": "/SmartBrain/static/js/runtime~main.65bb2954.js"
+  }
+]);
